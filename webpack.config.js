0fbde4c341ae40aecd7835741d170bed25706c7e
Jim Yang <jim@prosewell.com>
2017-12-27 16:50:20 -0500

    setting up webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,49 @@
 const webpack = require('webpack');
 const path = require('path');
 const CopyWebPackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+const config = {
+  context: path.join(__dirname),
+  entry: './client/src/client.js',
+  output: {
+    path: path.join(__dirname, '/client/dist/'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'es2015']
+        },
+        exclude: path.join(__dirname, '/node_modules')
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader!postcss-loader'
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("client.css"),
+    new CopyWebPackPlugin([
+      { from: './client/src/index.html', to: '../dist/index.html'}
+    ]),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      comments: false,
+      compress: {
+         warnings: false,
+         drop_console: false
+       }
+    })
+  ],
+  devtool: 'cheap-sourcemap'
+}
+
+module.exports = config;
