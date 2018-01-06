869d2fa619650f9d1cba42cf2a91cd17f13e6783
Jim Yang <jim@prosewell.com>
2018-01-05 23:13:28 -0500

    adding comments for future components

--- client/src/components/mailGenerator.js
+++ client/src/components/mailGenerator.js
@@ -3,11 +3,24 @@ import PersonGenerator from '../lib/personGenerator';
 
 let personGenerator = new PersonGenerator();
 
+class LetterContent {
+  // there are several types of requests...
+  // revenge, assassination, fame, treasure, brutality, mercy, power, pleasure?
+  // randomize request content
+}
+
+class RecipeGenerator {
+
+}
+
+class MaterialGenerator {
+  // these might not be random
+}
+
 class Letter {
-  constructor(subject, content, recipes = [], materials = [], quest = {}) {
+  constructor(townLevel, content, recipes = [], materials = [], quest = {}) {
     this.unread = true
     this.from = personGenerator.create()
-    this.subject = ''
     this.content = content
     this.recipes = recipes
     this.materials = materials
@@ -36,8 +49,8 @@ export default class mailGenerator {
     if (state.swords && state.swords[0] && state.swords[0].id === 'swordAmmit' && !state.swords[0].questFlag) {
       console.log('load ammit mails')
       state.swords[0].questFlag = true;
-      let villageBoyMail = new Letter('Please sir', 'I want fame.')
-      let envoyMail = new Letter('An offer', 'The sword')
+      let villageBoyMail = new Letter(0, 'I want fame.')
+      let envoyMail = new Letter(0, 'The sword')
       mail = [villageBoyMail, envoyMail]
     } else {
 
--- client/src/components/questGenerator.js
+++ client/src/components/questGenerator.js
@@ -1,7 +1,20 @@
 class quest {
-  constructor()
+  // quests can represent 9 categories of beings.
+  // natural >> demon, human, angel
+  // magical >> demon, human, angel
+  // spiritual >> demon, huma, angel
 }
 
+// natural <<>> magical <<>> spiritual
+// demonic <<>> human <<>> angelic
+// starting with weaker level swords
+// warriors killing these give fragments of the type.
+// mail is generated based on randomness.
+  // when a mail is generated it will take into account number of swords, types, etc?
+
 export default class questGenerator {
+  constructor() {
+    super()
+  }
 
 }
